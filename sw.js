53d15e9877bc4561ecff69d17010ebe43b2a60c6
liyyin <66932682+liyyin@users.noreply.github.com>
2020-06-15 11:11:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,10 +4,15 @@ self.addEventListener('install', function(event) {
       .then(function(cache) {
         cache.addAll([
           '/',
-          '/manifest.json',
           '/index.html',
+          '/manifest.json',
           '/src/css/app.css',
           '/src/js/app.js'
+          '/src/icons/icon-96x96.png'
+          '/src/icons/icon-144x144.png'
+          '/src/icons/icon-192x192.png'
+          '/src/icons/icon-384x384.png'
+          '/src/icons/icon-512x512.png'
         ])
       })
   );
